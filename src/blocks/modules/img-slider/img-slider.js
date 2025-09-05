85083410b32e23821a2951d9c4a01c69788ce4d2
FrontMD <web@mockup.digital>
2025-09-05 11:26:18 +0300

    fixes

--- src/blocks/modules/img-slider/img-slider.js
+++ src/blocks/modules/img-slider/img-slider.js
@@ -3,25 +3,27 @@ function imgSliderInit() {
 
     if(imgSliderBlocks.length < 0) return
 
-    imgSliderBlocks.forEach(block => {
-        const slider = block.querySelector('[data-js="imgSlider"]')
-        const slides = slider.querySelectorAll('[data-js="imgSliderSlide"]')
+    window.onload  = function() { 
+        imgSliderBlocks.forEach(block => {
+            const slider = block.querySelector('[data-js="imgSlider"]')
+            const slides = slider.querySelectorAll('[data-js="imgSliderSlide"]')
 
-        slides.forEach(slide => {
-            const img = slide.querySelector('[data-js="imgSliderImg"]')
-            const imgWidth = img.offsetWidth
-            slide.style.width = imgWidth + 'px'
-        })
-        
-        const sliderEx = new Swiper(slider, {
-            slidesPerView: 'auto',
-            spaceBetween: 10,
-            breakpoints: {
-                1280: {
-                    spaceBetween: 24
+            slides.forEach(slide => {
+                const img = slide.querySelector('[data-js="imgSliderImg"]')
+                const imgWidth = img.offsetWidth
+                slide.style.width = imgWidth + 'px'
+            })
+            
+            const sliderEx = new Swiper(slider, {
+                slidesPerView: 'auto',
+                spaceBetween: 10,
+                breakpoints: {
+                    1280: {
+                        spaceBetween: 24
+                    }
                 }
-            }
+            })
+            
         })
-
-    })
+    };
 }
