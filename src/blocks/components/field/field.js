b5dd2b724d380a131e07a29b81f9c0611e007d9c
FrontMD <web@mockup.digital>
2025-11-13 12:29:42 +0300

    selects

--- src/blocks/components/field/field.js
+++ src/blocks/components/field/field.js
@@ -40,14 +40,51 @@ function selects() {
 	formSelects.forEach(formSelect => {
 		let placeholder = $(formSelect).attr('data-placeholder')
 
-		$(formSelect).select2({
-			placeholder: placeholder,
-			allowClear: true
-		});
+		if(formSelect.hasAttribute('data-filterable')) {
+			$(formSelect).select2({
+				placeholder: placeholder,
+				allowClear: true,
+				templateResult: function (data) {
+					if(data.element && $(data.element).hasClass('disabled')) {
+						return null;
+					}
+					return data.text;
+				}
+			});
+		} else {
+			$(formSelect).select2({
+				placeholder: placeholder,
+				allowClear: true
+			});
+		}
 	}) 
 
 	$("[data-js='formSelect']").on('select2:open', () => {
 		$(".select2-dropdown").addClass("select2-dropdown--form")
 	})
 
+}
+
+function reloadSelect(formSelect) {
+	if(formSelect) {
+		let placeholder = $(formSelect).attr('data-placeholder')
+		$(formSelect).select2('destroy');
+		if(formSelect.hasAttribute('data-filterable')) {
+			$(formSelect).select2({
+				placeholder: placeholder,
+				allowClear: true,
+				templateResult: function (data) {
+					if(data.element && $(data.element).hasClass('disabled')) {
+						return null;
+					}
+					return data.text;
+				}
+			});
+		} else {
+			$(formSelect).select2({
+				placeholder: placeholder,
+				allowClear: true
+			});
+		}
+	}
 }
