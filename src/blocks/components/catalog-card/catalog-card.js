70d670d18818d97edc0177a0cf4912f0aab4879d
FrontMD <web@mockup.digital>
2025-12-17 12:10:35 +0300

    fix

--- src/blocks/components/catalog-card/catalog-card.js
+++ src/blocks/components/catalog-card/catalog-card.js
@@ -4,23 +4,25 @@ function ccSlidersInit() {
     if(ccSliders.length < 1) return
 
     ccSliders.forEach(slider => {
-        const pagination = slider.querySelector('[data-js="catalogCardPagination"]')
-
-        let sliderEx = new Swiper(slider, {
-            slidesPerView: 1.148,
-            spaceBetween: 10,
-            breakpoints: {
-                768: {
-                   slidesPerView: 1,
-                    spaceBetween: 12, 
-                }
-            },
-            pagination: {
-                el: pagination,
-                type: 'bullets',
-                clickable: true
-            },
-
-        })
+        if (!slider.swiper) {
+            const pagination = slider.querySelector('[data-js="catalogCardPagination"]')
+    
+            let sliderEx = new Swiper(slider, {
+                slidesPerView: 1.148,
+                spaceBetween: 10,
+                breakpoints: {
+                    768: {
+                       slidesPerView: 1,
+                        spaceBetween: 12, 
+                    }
+                },
+                pagination: {
+                    el: pagination,
+                    type: 'bullets',
+                    clickable: true
+                },
+    
+            })
+        }
     })
 }
