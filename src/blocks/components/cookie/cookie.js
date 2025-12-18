3c6f169342970a3381c442a65179337ebfcaf28a
FrontMD <web@mockup.digital>
2025-12-18 16:35:46 +0300

    cookie, brands list, footer fix

--- src/blocks/components/cookie/cookie.js
+++ src/blocks/components/cookie/cookie.js
@@ -5,15 +5,25 @@ function cookieInit() {
 
     const cookieBtn = cookieMessage.querySelector('[data-js="cookieBtn"]');
     const cookieName = 'cookie_policy'
+    const cookieLifespan = 604800000
 
     let currentCookieValue = document.cookie.match(new RegExp("(?:^|; )" + cookieName.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"));
 
-    if (!currentCookieValue) {
+    if(currentCookieValue) {
+        const lastAcept = parseInt(currentCookieValue[1])
+        const now = new Date().getTime();
+
+        if(now - lastAcept >= cookieLifespan) {
+            cookieMessage.classList.add('active');
+        } 
+
+    } else {
         cookieMessage.classList.add('active');
     }
 
     cookieBtn.addEventListener('click', function () {
-        document.cookie = `${cookieName}=true`
+        const timestamp = new Date().getTime();
+        document.cookie = `${cookieName}=${timestamp}`
         cookieMessage.classList.remove('active');
     });
 }
